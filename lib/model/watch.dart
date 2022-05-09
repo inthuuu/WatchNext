39218f4ba86890629dab9b8fc0d9ad321d63cc3f
inthuu <ploy.mark1122@gmail.com>
2022-05-09 11:51:30 +0700

    main

---
+++ lib/model/watch.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: camel_case_types
+
+class watch {
+  String name;
+  String imgURL;
+  String link;
+
+  watch({required this.name, required this.imgURL, required this.link});
+}
